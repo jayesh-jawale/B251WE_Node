6a44dd651cae8c303416230292866c58f6e5b437
jayesh-jawale <jawalejayesh123@gmail.com>
2022-02-02 23:26:37 +0530

    jwt provided

--- movies.js
+++ movies.js
@@ -2,6 +2,7 @@ import express from "express";
 import { getMoviesByID, getMoviesByRating, createMovies, updateMovie, deleteMovieByIDAndName, deleteMovieByID }
  from "./helper.js";
  import { auth } from "./middleware/auth.js";
+ import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
